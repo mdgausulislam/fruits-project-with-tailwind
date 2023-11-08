4f657e3b81cf4f3c74cd9863c213bbad1e2e79ef
unknown <gausulislam5@gmail.com>
2023-11-08 08:50:32 +0600

    create router and components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,8 @@ module.exports = {
   env: { browser: true, es2020: true,node:true },
   extends: [
     'eslint:recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
